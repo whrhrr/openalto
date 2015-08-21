88984bfa353820139d83c88b71cb0609dbd62762
dongshu <dongs2011@gmail.com>
2015-08-21 23:10:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+##Install dependencies
+
+Run `npm install` to install dependencies
+
+##Start the Server
+
+Run `node app.js` to start the web server
+
+##Start the Server with monitoring
+
+Run `npm install -g nodemon` to install the nodemon package
+Run `nodemon app.js` to start and monitor the server
---
+++ test/app.js
@@ -0,0 +1,26 @@
+var request = require('supertest');
+var app = require('../app.js');
+
+describe('GET /', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
+
+describe('GET /contact', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/contact')
+      .expect(200, done);
+  });
+});
+
+describe('GET /random-url', function() {
+  it('should return 404', function(done) {
+    request(app)
+      .get('/reset')
+      .expect(404, done);
+  });
+});
