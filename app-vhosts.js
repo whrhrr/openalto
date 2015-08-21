2ba59559ce30210212155e81fbbb7ef394ce4393
dongshu <dongs2011@gmail.com>
2015-08-21 15:50:43 +0800

    add comments

--- app-vhosts.js
+++ app-vhosts.js
@@ -1,5 +1,4 @@
 var express = require('express');
-// var connect = require('connect');
 var cookieParser = require('cookie-parser');
 var compress = require('compression');
 var favicon = require('serve-favicon');
@@ -20,55 +19,37 @@ var engines = require('consolidate');
  **/
 var homeController = require('./controllers/home');
 
-var appInterop = express();
-
-var evh = require('express-vhost');
-var app = express();
-var port = 9000;
-
-//Express configuration
-// appInterop.set('port', process.env.PORT || 3000);
-appInterop.set('views', path.join(__dirname, 'views'));
-appInterop.engine('html', engines.hogan);
-appInterop.set('view engine', 'html');
-appInterop.use(compress());
-appInterop.use(assets({
-      paths: ['public/css', 'public/js']
-}));
-
-appInterop.use(logger('dev'));
-appInterop.use(favicon(path.join(__dirname, 'public/images/favicon.png')));
-appInterop.use(bodyParser.urlencoded({ extended: true }));
-appInterop.use(expressValidator());
-appInterop.use(cookieParser());
-
-appInterop.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
-
 /**
- * Primary AppInterop Routes
+ * openalto.org
  **/
-appInterop.get('/', homeController.index);
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', engines.hogan);
+app.set('view engine', 'html');
+app.use(compress());
+app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'public/images/favicon.png')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use(errorHandler());
+app.get('/', homeController.index);
 
 /**
- * Error Handler.
- */
-appInterop.use(errorHandler());
-
-/**
- * Start Express server.
- */
-// appInterop.listen(appInterop.get('port'), function() {
-//       console.log('Express server listening on port %d in %s mode', appInterop.get('port'), appInterop.get('env'));
-// });
-
-app.use(evh.vhost(app.enabled('trust proxy')));
-app.listen(port);
-evh.register('openalto.org', appInterop);
-
+ * blog.openalto.org
+ **/
 var appBlog = express();
 appBlog.get('/', function(req, res) {
     res.send("Welcome to our blog!");
 });
+
+var main = express();
+var evh = require('express-vhost');
+var port = process.env.PORT || 9000;
+main.use(evh.vhost(main.enabled('trust proxy')));
+main.listen(port);
+evh.register('openalto.org', app);
 evh.register('blog.openalto.org', appBlog);
 
-module.exports = app;
+module.exports = main;
