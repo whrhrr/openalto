3af453d2452f85f8f7415e999d960864d7cb2b63
xiaolin <linxiao9292@outlook.com>
2015-09-28 17:35:06 +0800

    change alto server&client layout

--- routers/main-router.js
+++ routers/main-router.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var index = require('../lib/data/index.js')
+var interop_index = require('../lib/data/interop-index.js')
 
 router.use(function(req, res, next) {
   console.log('%s %s %s', req.method, req.url, req.path);
   next();
 });
 
-router.get('/', function(req, res){
+router.get('/', function(req, res, next){
   var data = index.home_data(req, res);
   res.render('main/index', data);
+});
+
+router.get('/interop', function(req, res, next){
+  res.render('interop/index', {
+    title: "Interop"
+  });
+//  var data = index.home_data(req, res);
+//  res.render('main/index', data);
+});
+
+router.post('/test', function(req, res, next){
+  interop_index.get_test_data(req, res, function(stdout) {
+    res.send(stdout);
+  });
 });
 
 module.exports = router
--- servers/main-server.js
+++ servers/main-server.js
@@ -2,6 +2,9 @@ var server = require('./server');
 var app = server.create();
 
 var router = require('../routers/main-router');
-app.get('/', router);
-
+//app.get('/', router);
+//app.get('/interop', router);
+//app.post('/test', router);
+app.use('/', router);
 module.exports = app;
+
